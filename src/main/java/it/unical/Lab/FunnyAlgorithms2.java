f4f691ed31a2909f96efa62ef5b374f200c32fac
11Nait11 <115213428+11Nait11@users.noreply.github.com>
2022-10-28 16:55:19 +0200

    implementato test selectionSortCrescenteWorks

--- src/main/java/it/unical/Lab/FunnyAlgorithms2.java
+++ src/main/java/it/unical/Lab/FunnyAlgorithms2.java
@@ -11,7 +11,7 @@ public class FunnyAlgorithms2 {
 	 * @return Index of target in nums array, -1 otherwise
 	 */
 	public int binarySearch(int[] nums, int target) {
-		int left = 1, right = nums.length - 2;
+		int left = 0, right = nums.length - 1;
 
 		while (left <= right) {
 			int mid = left + (right - left) / 2;
@@ -22,7 +22,7 @@ public class FunnyAlgorithms2 {
 			} else if (target < nums[mid]) {
 				right = mid - 1;
 			} else {
-				left = mid + 2;
+				left = mid + 1;
 			}
 		}
 		// `target` doesn't exist in the array
@@ -41,13 +41,13 @@ public class FunnyAlgorithms2 {
 	 * @param array Array to be sorted
 	 * @param order Direction to sort the array (0:Ascending, 1:Descending)
 	 */
-	public void selectionSort(int[] array, int order) {
+	public int[] selectionSort(int[] array, int order) {
 
 		if (order != 0 && order != 1) {
 			throw new IllegalArgumentException("L'ordine può essere 0 o 1.");
 		}
 
-		for (int i = 0; i < array.length - 2; i++) {
+		for (int i = 0; i < array.length-1; i++) {
 			int min = i;
 			for (int j = i + 1; j < array.length; j++) {
 				boolean orderCondition = order == 0 ? array[j] > array[min] : array[j] < array[min];
@@ -57,6 +57,7 @@ public class FunnyAlgorithms2 {
 			}
 			swap(array, min, i);
 		}
+		return array;
 	}
 
 	/**
--- src/test/java/it/unical/Lab/FunnyAlgorithms2Test.java
+++ src/test/java/it/unical/Lab/FunnyAlgorithms2Test.java
@@ -1,7 +1,10 @@
 package it.unical.Lab;
 
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+
+import java.util.Arrays;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -25,4 +28,38 @@ public class FunnyAlgorithms2Test
     	assertEquals(1,fa.stringToIntConverter("1"));
     	assertEquals(10,fa.stringToIntConverter("10"));
     }
+    @Test
+    public void binarySearchWork()
+    {
+    	int[] nums= {3,4,9,1,8,4};
+    	assertEquals(0,fa.binarySearch(nums,3));
+    }
+    
+    @Test
+    public void binarySearchNotWork()
+    {
+    	int[] nums= {3,4,9,1,8,4};
+    	assertEquals(-1,fa.binarySearch(nums,10));
+    }
+    
+  
+    
+    @Test
+	public void selectionSortCrescenteWorks() {
+		int[] array = {5,2,3,9,6};
+		int[] order = {2,3,5,6,9};
+		
+		assertArrayEquals(order, fa.selectionSort(array, 1));
+	}
+    
+    @Test
+   	public void selectionSortCrescenteNotWorks() {
+   		int[] array = {5,2,3,9,6};
+   	
+   		int[] reverse= {9,6,5,3,2};
+   		
+   		assertArrayEquals(reverse, fa.selectionSort(array, 0));
+   	}
+       
+    
 }
